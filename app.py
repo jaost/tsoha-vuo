3bc6a2475ca8d52142fa54cfd655d75be39615bd
Jani Österman <jani@alasti.fi>
2020-09-21 00:04:36 +0300

    App checks if upload folder does not exist, creates the folder if not.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from os import getenv
+import os
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+
+if not os.path.exists('static/uploads'):
+    os.makedirs('static/uploads')
 
 import routes
