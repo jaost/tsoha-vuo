cdf71efcfa9758e83cd8217c05f0ed24893ab9d4
Jani Österman <jani@alasti.fi>
2020-10-18 22:58:36 +0300

    Added description to forms Formated code Highlighted feed & item create buttons.

--- feeds.py
+++ feeds.py
@@ -1,109 +1,142 @@
-from db import db
-import users
 import os
+
+import users
+from db import db
+
 
 def create_feed(secret, title, description):
     user_id = users.user_id()
     if user_id == 0:
         return False
-    sql = "INSERT INTO feeds (secret, title, description, user_id, created_at) VALUES (:secret, :title, :description, :user_id, NOW())"
-    db.session.execute(sql, {"secret":secret, "title":title, "description": description, "user_id":user_id})
+    sql = "INSERT INTO feeds (secret, title, description, user_id, created_at) " \
+          "VALUES (:secret, :title, :description, :user_id, NOW())"
+    db.session.execute(
+        sql,
+        {
+            "secret": secret,
+            "title": title,
+            "description": description,
+            "user_id": user_id,
+        },
+    )
     db.session.commit()
     return True
+
 
 def delete_feed(secret):
     user_id = users.user_id()
     if user_id == 0:
         return False
     sql = "SELECT * FROM feeds WHERE secret=:secret"
-    feed = db.session.execute(sql, {"secret":secret}).fetchone()
-    if user_id != feed['user_id']:
+    feed = db.session.execute(sql, {"secret": secret}).fetchone()
+    if user_id != feed["user_id"]:
         return False
-        
+
     sql = "SELECT * FROM items WHERE feed_id=:feed_id"
-    items = db.session.execute(sql, {"feed_id":feed['id'] }).fetchall()
+    items = db.session.execute(sql, {"feed_id": feed["id"]}).fetchall()
 
     for item in items:
-        if os.path.exists(item['path']):
-            os.remove(item['path'])
+        if os.path.exists(item["path"]):
+            os.remove(item["path"])
 
     sql = "DELETE from items WHERE feed_id=:feed_id"
-    db.session.execute(sql, {"feed_id":feed['id'] })
+    db.session.execute(sql, {"feed_id": feed["id"]})
     db.session.commit()
     sql = "DELETE from feeds WHERE id=:id"
-    db.session.execute(sql, {"id":feed['id'] })
+    db.session.execute(sql, {"id": feed["id"]})
     db.session.commit()
     return True
 
+
 def get_feed(secret):
     sql = "SELECT * FROM feeds WHERE secret=:secret"
-    result = db.session.execute(sql, {"secret":secret})
+    result = db.session.execute(sql, {"secret": secret})
     return result.fetchone()
+
 
 def get_feeds():
     sql = "SELECT * FROM feeds WHERE user_id=:user_id"
-    result = db.session.execute(sql, { "user_id":users.user_id() })
+    result = db.session.execute(sql, {"user_id": users.user_id()})
     return result.fetchall()
 
+
 def get_items(secret):
-    sql = "SELECT I.id as id, I.description as description, I.path as path, I.created_at as created_at, I.user_id as user_id FROM items I, feeds F WHERE I.feed_id=F.id AND F.secret=:secret ORDER BY I.created_at DESC"
-    result = db.session.execute(sql, {"secret":secret})
+    sql = "SELECT I.id as id, I.description as description, I.path as path, I.created_at as created_at, I.user_id as user_id  " \
+          "FROM items I, feeds F WHERE I.feed_id=F.id AND F.secret=:secret ORDER BY I.created_at DESC"
+    result = db.session.execute(sql, {"secret": secret})
     return result.fetchall()
+
 
 def create_item(secret, path, description):
     user_id = users.user_id()
     if user_id == 0:
         return False
     sql = "SELECT id FROM feeds WHERE secret=:secret"
-    result = db.session.execute(sql, {"secret":secret})
+    result = db.session.execute(sql, {"secret": secret})
     feed = result.fetchone()
     if feed == None:
         return False
-    sql = "INSERT INTO items (feed_id, path, description, user_id, created_at) VALUES (:feed_id, :path, :description, :user_id, NOW())"
-    db.session.execute(sql, {"feed_id":feed[0], "path": path, "description": description, "user_id":user_id})
+    sql = "INSERT INTO items (feed_id, path, description, user_id, created_at) " \
+          "VALUES (:feed_id, :path, :description, :user_id, NOW())"
+    db.session.execute(
+        sql,
+        {
+            "feed_id": feed[0],
+            "path": path,
+            "description": description,
+            "user_id": user_id,
+        },
+    )
     db.session.commit()
     return True
+
 
 def delete_item(secret, id):
     user_id = users.user_id()
     if user_id == 0:
         return False
     sql = "SELECT id, user_id FROM feeds WHERE secret=:secret"
-    result = db.session.execute(sql, {"secret":secret})
+    result = db.session.execute(sql, {"secret": secret})
     feed = result.fetchone()
     if feed == None:
         return False
     sql = "SELECT id, user_id, path FROM items WHERE feed_id=:feed_id AND id=:id"
-    result = db.session.execute(sql, {"feed_id":feed[0], "id":id })
+    result = db.session.execute(sql, {"feed_id": feed[0], "id": id})
     item = result.fetchone()
-    if feed['user_id'] != user_id and item['user_id'] != user_id:
+    if feed["user_id"] != user_id and item["user_id"] != user_id:
         return False
-    if os.path.exists(item['path']):
-        os.remove(item['path'])
+    if os.path.exists(item["path"]):
+        os.remove(item["path"])
     sql = "DELETE FROM items WHERE feed_id=:feed_id AND id=:id"
-    db.session.execute(sql, {"feed_id":feed[0], "id":id })
+    db.session.execute(sql, {"feed_id": feed[0], "id": id})
     db.session.commit()
     return True
-    
+
+
 def get_votes(item_id):
     sql = "SELECT id FROM votes WHERE feed_id=:feed_id AND item_id=:item_id"
-    result = db.session.execute(sql, {"secret":secret})
+    result = db.session.execute(sql, {"secret": secret})
     return result.fetchall()
+
 
 def vote_item(secret, item_id):
     user_id = users.user_id()
     sql = "SELECT id FROM items I, feeds F WHERE I.feed_id=F.id AND F.secret=:secret"
-    result = db.session.execute(sql, {"secret":secret, "id":item_id})
+    result = db.session.execute(sql, {"secret": secret, "id": item_id})
     item = result.fetchone()
     if user_id == 0 or item == None:
         return False
     sql = "SELECT id FROM votes WHERE feed_id=:feed_id AND item_id=:item_id AND user_id=:user_id"
-    result = db.session.execute(sql, {"feed_id":feed_id, "item_id":item_id, "user_id":user_id})
+    result = db.session.execute(
+        sql, {"feed_id": feed_id, "item_id": item_id, "user_id": user_id}
+    )
     vote = result.fetchone()
     if vote == None:
         sql = "INSERT INTO votes (feed_id, item_id, user_id) VALUES (:content, :user_id, NOW())"
     else:
         sql = "DELETE FROM votes WHERE feed=:feed_id AND item_id=:item_id AND user_id=:user_id"
-    db.session.execute(sql, {"feed_id":feed_id, "item_id":item_id, "user_id":user_id})
+    db.session.execute(
+        sql, {"feed_id": feed_id, "item_id": item_id, "user_id": user_id}
+    )
     db.session.commit()
     return True
--- routes.py
+++ routes.py
@@ -5,20 +5,21 @@ from os import path
 from werkzeug.utils import secure_filename
 from slugify import slugify
 
-import calendar;
-import time;
+import calendar
+import time
 
 
 @app.route("/")
 def index():
-    
+
     if users.user_id() != 0:
         feed_list = feeds.get_feeds()
     else:
         feed_list = []
     return render_template("index.html", feed_list=feed_list)
 
-@app.route('/feed', methods=["get","post"])
+
+@app.route("/feed", methods=["get", "post"])
 def create_feed():
     error = []
     if request.method == "GET":
@@ -34,28 +35,39 @@ def create_feed():
         if len(description) < 5 or len(description) > 199:
             error.append("Description should be more than 4 and under 200 characters.")
         if len(error) == 0 and feeds.create_feed(secret, title, description):
-            return redirect("/feed/"+secret)
+            return redirect("/feed/" + secret)
         else:
             error.append("Could not create feed. Try with different secret.")
-            return render_template("new_feed.html", secret=secret, title=title, description=description, error=error)
+            return render_template(
+                "new_feed.html",
+                secret=secret,
+                title=title,
+                description=description,
+                error=error,
+            )
 
-@app.route('/feed/<string:secret>/<int:id>', methods=["post", "delete"])
+
+@app.route("/feed/<string:secret>/<int:id>", methods=["post", "delete"])
 def modify_item(secret, id):
     if request.method == "POST":
-        if request.form.get("action", False) == "delete" and feeds.delete_item(secret, id):
-            return redirect("/feed/"+secret)
+        if request.form.get("action", False) == "delete" and feeds.delete_item(
+            secret, id
+        ):
+            return redirect("/feed/" + secret)
         if request.form.get("action", False) == "vote" and feeds.vote_item(secret, id):
-            return redirect("/feed/"+secret+"#I-"+id)
+            return redirect("/feed/" + secret + "#I-" + id)
         else:
             abort(400)
     else:
-        return redirect("/feed/"+secret)
+        return redirect("/feed/" + secret)
 
-@app.route('/feed/<string:secret>/new')
+
+@app.route("/feed/<string:secret>/new")
 def create_item(secret):
-    return render_template("new_item.html", secret=secret) ##, image=request.args.get('image'), description=request.args.get('description'), error=request.args.get('error'))
+    return render_template("new_item.html", secret=secret)
 
-@app.route('/feed/<string:secret>', methods=["get","post"])
+
+@app.route("/feed/<string:secret>", methods=["get", "post"])
 def feed(secret):
     feed = feeds.get_feed(secret)
     error = []
@@ -71,52 +83,76 @@ def feed(secret):
         if request.form.get("action", False) == "delete":
             if feeds.delete_feed(secret):
                 return redirect("/")
-            return render_template("feed.html", secret=secret, error=["Could not delete item."])
+            return render_template(
+                "feed.html", secret=secret, error=["Could not delete item."]
+            )
         elif request.form.get("action", False) == "new_item":
-            if not request.files['image']:
+            if not request.files["image"]:
                 error.append("No image found.")
             feed = feeds.get_feed(secret)
             if feed == None or feed == 0 or users.user_id() == 0:
                 error.append("Feed not found or user not logged in.")
             description = request.form["description"]
             if len(description) < 3 or len(description) > 199:
-                error.append("Description should be more than 3 and under 200 characters.")
-            uploaded_file = request.files['image']
+                error.append(
+                    "Description should be more than 3 and under 200 characters."
+                )
+            uploaded_file = request.files["image"]
             filename = uploaded_file.filename
             if len(filename) < 3 or len(filename) > 30:
                 error.append("Filename should be between 3 and 30 characters.")
             file_ext = path.splitext(filename)[1]
-            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            if file_ext not in app.config["UPLOAD_EXTENSIONS"]:
                 error.append("Filetype is not supported")
             if len(error) > 0:
-                return render_template("new_item.html", secret=secret, image=request.files['image'], description=description, error=error)
-            file_path = path.join('static/uploads', secure_filename(secret + '-' + str(calendar.timegm(time.gmtime())) + '-' +filename))
+                return render_template(
+                    "new_item.html",
+                    secret=secret,
+                    image=request.files["image"],
+                    description=description,
+                    error=error,
+                )
+            file_path = path.join(
+                "static/uploads",
+                secure_filename(
+                    secret + "-" + str(calendar.timegm(time.gmtime())) + "-" + filename
+                ),
+            )
             uploaded_file.save(file_path)
             if feeds.create_item(secret, file_path, description):
-                return redirect("/feed/"+secret)
+                return redirect("/feed/" + secret)
             else:
-                return render_template("new_item.html", secret=secret, error=["Something went wrong. Try again later."])
+                return render_template(
+                    "new_item.html",
+                    secret=secret,
+                    error=["Something went wrong. Try again later."],
+                )
         else:
             return render_template("feed.html", feed=feed, error=["Does not work :("])
 
-@app.route("/login", methods=["get","post"])
+
+@app.route("/login", methods=["get", "post"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        if users.login(username,password):
+        if users.login(username, password):
             return redirect("/")
         else:
-            return render_template("login.html", username=username, error=["Wrong username / password"])
+            return render_template(
+                "login.html", username=username, error=["Wrong username / password"]
+            )
+
 
 @app.route("/logout")
 def logout():
     users.logout()
     return redirect("/")
 
-@app.route("/signup", methods=["get","post"])
+
+@app.route("/signup", methods=["get", "post"])
 def signup():
     if request.method == "GET":
         return render_template("signup.html")
@@ -129,7 +165,7 @@ def signup():
         if len(username) < 4 or len(password) < 4:
             error.append("Username & password should be over 3 characters.")
 
-        if len(error) == 0 and users.register(username,password):
+        if len(error) == 0 and users.register(username, password):
             return redirect("/")
         else:
             error.append("Failed create account.")
